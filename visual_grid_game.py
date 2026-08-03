65b17c9fc35c4b5d1fc9b90380aeaac4dddf4f76
shehanweersing <shehanweersing@gmail.com>
2026-08-03 16:44:22 +0530

    iterate through traps to render custom purple shapes on the canvas.

--- visual_grid_game.py
+++ visual_grid_game.py
@@ -87,7 +87,7 @@ class VisualGridHuntGame:
 
         if tuple_pos in self.toxic_traps:
             self.score -= 15
-            
+
         for op in self.opponents:
             move = random.choice(['Up', 'Down', 'Left', 'Right', 'Stay'])
             if move == 'Up' and op[1] < self.height - 1:
@@ -161,6 +161,19 @@ class GridGameGUI:
             self.canvas.create_oval(x1, y1, x1 + self.cell_size * 0.5, y1 + self.cell_size * 0.5, fill="#f59e0b",
                                     outline="#d97706")
 
+        for tx, ty in self.env.toxic_traps:
+            offset = self.cell_size * 0.25
+            x1 = tx * self.cell_size + offset
+            y1 = (self.env.height - 1 - ty) * self.cell_size + offset
+
+            self.canvas.create_oval(
+                x1,
+                y1,
+                x1 + self.cell_size * 0.5,
+                y1 + self.cell_size * 0.5,
+                fill="purple",
+                outline="darkviolet"
+    )
         for ox, oy in self.env.opponents:
             offset = self.cell_size * 0.2
             x1 = ox * self.cell_size + offset
